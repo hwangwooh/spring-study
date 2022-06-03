758ffd7da2dc5e69e3ddb0a122f0bd4a7fe7afa7
hwh <zaq8077@gmail.com>
2022-06-03 20:17:12 +0900

    Initial commit

---
+++ src/test/java/scan/filter/BeanA.java
@@ -0,0 +1,4 @@
+package scan.filter;
+@MyincludeComponent
+public class BeanA {
+}
---
+++ src/test/java/scan/filter/ComponentFilterAppConpigTest.java
@@ -0,0 +1,40 @@
+package scan.filter;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.springframework.context.annotation.ComponentScan.*;
+
+public class ComponentFilterAppConpigTest {
+
+    @Test
+    void filerScan(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(CamponentFilterAppConfig.class);
+        BeanA beanA = ac.getBean("beanA", BeanA.class);
+        Assertions.assertThat(beanA).isNotNull();
+
+
+        assertThrows(
+                NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("beanB", BeanB.class));
+    }
+
+    @Configuration
+    @ComponentScan(
+            includeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyincludeComponent.class),
+            excludeFilters = @Filter(type = FilterType.ANNOTATION,classes = MyExcludeComponent.class)
+    )
+    static class CamponentFilterAppConfig{
+
+
+    }
+}
